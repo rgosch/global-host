4cb1c2f6b32cbcb330b14a02c6b372cc72ccfc96
Gosch <dev@gosch.io>
2020-07-07 13:14:25 -0300

    Implements Link to Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 
 import logoSVG from 'assets/img/logo-white.svg';
 import { TEXTS } from 'logic/texts';
+import { Link } from 'react-router-dom';
+import { ROUTES } from 'logic/constants';
 import { Container, Logo, Title } from './styles';
 
 function Header() {
   return (
     <Container>
       <Title>{TEXTS.title}</Title>
-      <Logo src={logoSVG} alt={TEXTS.title} />
+      <Link to={ROUTES.MAIN}>
+        <Logo src={logoSVG} alt={TEXTS.title} />
+      </Link>
     </Container>
   );
 }
